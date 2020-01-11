dd8d6f6dc445c6e8c5a97e0fcc030b71873421f8
Jack Yang <yuntianyang@ucsb.edu>
2020-01-11 15:07:38 -0800

    add file

---
+++ two_time_pad_attack.py
@@ -0,0 +1,23 @@
+import sys
+import re
+
+ct1 = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e"
+ct2 = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+
+
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+
+pos = []
+xored_ct = strxor(ct1.decode('hex'),ct2.decode('hex'))
+crib = " the "
+
+# keep guessing alternatively
+for i in range(len(xored_ct)-5):
+    xored_crib = strxor(crib, xored_ct[i:i+6])
+    if (bool(re.search('^[a-zA-Z0-9\., \'\"\-_\:\(\)]+$', xored_crib))):
+        print(xored_crib)
